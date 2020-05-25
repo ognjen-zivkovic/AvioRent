a30dca63f73f43dc4b9edeb395532788a606875e
ognjen-zivkovic <ozivkovich@gmail.com>
2020-05-25 20:58:17 +0200

    Initial commit

---
+++ src/main/java/com/aviorent/AviorentApplication.java
@@ -0,0 +1,13 @@
+package com.aviorent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AviorentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AviorentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aviorent/AviorentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aviorent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AviorentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
