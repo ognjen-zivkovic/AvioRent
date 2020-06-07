3a53fbc1aa838cf1cfca9d62fedbb02a7935afd2
Luka <luka99.r@gmail.com>
2020-06-07 14:21:03 +0200

    CrewMember add CrewMember

--- src/main/java/com/aviorent/controllers/CrewMemberController.java
+++ src/main/java/com/aviorent/controllers/CrewMemberController.java
@@ -1,13 +1,18 @@
 package com.aviorent.controllers;
 
 import com.aviorent.models.CrewMember;
+import com.aviorent.models.CrewMemberType;
 import com.aviorent.services.CrewMemberService;
 import com.aviorent.services.CrewMemberTypeService;
+import com.aviorent.services.PlaneService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -16,10 +21,48 @@ public class CrewMemberController {
     @Autowired
     private CrewMemberService crewMemberService;
 
-    @GetMapping("/crewMembers")
+    @Autowired
+    private CrewMemberTypeService crewMemberTypeService;
+
+    @Autowired
+    private PlaneService planeService;
+
+    @GetMapping("/crewmemberlist")
     public String crewMembers(Model model){
         List<CrewMember> members = this.crewMemberService.getAll();
         model.addAttribute("members", members);
-        return "crewMembers";
+        return "crewmemberlist";
     }
+
+    @RequestMapping("/crewmemberadd")
+    public String createCrewMember(Model model){
+
+        model.addAttribute("crewMember", new CrewMember());
+        model.addAttribute("type", crewMemberTypeService.getAll());
+        model.addAttribute("plane", planeService.getAll());
+
+
+        return "/crewmemberadd";
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String saveCrewMember(@Valid CrewMember crewMember, BindingResult bindingResult, Model model){
+
+        if(bindingResult.hasErrors()){
+            return "/crewmemberadd";
+        }
+        else{
+            CrewMember savedCrewMember = crewMemberService.save(crewMember);
+            return "redirect:/crewmemberlist";
+        }
+    }
+
+   /* @RequestMapping("/admin/post/delete/{id}")
+    public String delete(@PathVariable Long id, RedirectAttributes redirectAttrs) {
+        postService.delete(id);
+        redirectAttrs.addFlashAttribute("message", "Post was deleted!");
+        return "redirect:/admin/posts";
+    }*/
+
+
 }
