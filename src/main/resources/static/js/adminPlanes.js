9e9b1f902514f0f40a4d81b71fa3a5d1f1c196e2
ognjen-zivkovic <ozivkovich@gmail.com>
2020-06-09 13:28:05 +0200

    added pagination, search and sorting to the planeAdmin datatable

--- src/main/resources/static/js/adminPlanes.js
+++ src/main/resources/static/js/adminPlanes.js
@@ -6,6 +6,20 @@ $(function () {
         getRow(id);
     });
 
+    $(function () {
+        $('#example1').DataTable({
+            responsive: true
+        })
+        $('#example2').DataTable({
+            'paging'      : true,
+            'lengthChange': false,
+            'searching'   : false,
+            'ordering'    : true,
+            'info'        : true,
+            'autoWidth'   : false
+        })
+    })
+
 
     $(document).on('click', '#image', function (e) {
 
